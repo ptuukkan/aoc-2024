0f1976b0a27cea0e156c9884a3834d15703bff27
Pekka Tuukkanen <petuukka@gmail.com>
2024-12-15 20:19:58 +0100

    day13 finish lol

--- src/day13/day13.go
+++ src/day13/day13.go
@@ -2,6 +2,7 @@ package day13
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 	"strings"
@@ -87,5 +88,121 @@ func Part1(input string) string {
 }
 
 func Part2(input string) string {
-	return ""
+	clawMachines := parseInput(input)
+
+	cost := int64(0)
+	for m, clawMachine := range clawMachines {
+		fmt.Printf("Clawmachine %d\n", m)
+		price_x := int64(clawMachine.Price.X) + 10000000000000
+		price_y := int64(clawMachine.Price.Y) + 10000000000000
+		a_x := int64(clawMachine.ButtonA.X)
+		a_y := int64(clawMachine.ButtonA.Y)
+		b_x := int64(clawMachine.ButtonB.X)
+		b_y := int64(clawMachine.ButtonB.Y)
+
+		i := int64(0)
+		curr_x := i * a_x
+		curr_y := i * a_y
+		diff_x := price_x - curr_x
+		diff_y := price_y - curr_y
+		count_x := diff_x / b_x
+		count_y := diff_y / b_y
+
+		delta_count_x := int64(-1)
+		delta_count_y := int64(-1)
+		delta_i := int64(-1)
+
+		first_count_x := int64(-1)
+		first_count_y := int64(-1)
+		first_i := int64(-1)
+
+		for {
+			// if curr_x > price_x || curr_y > price_y || count_y < i {
+			// 	fmt.Printf("count_x %d - count_y %d - i %d\n", count_x, count_y, i)
+			// 	fmt.Printf("curr_x %d - curr_y %d\n", curr_x, curr_y)
+			// 	fmt.Printf("curr_x > price_x: %v", curr_x > price_x)
+			// 	fmt.Printf("curr_y > price_y: %v", curr_y > price_y)
+			// 	fmt.Printf("count_y < i: %v", count_y < i)
+			// 	fmt.Printf("count_x < i: %v", count_x < i)
+			// 	break
+			// }
+			if i > 1000000 {
+				break
+			}
+
+			curr_x += a_x
+			curr_y += a_y
+			diff_x = price_x - curr_x
+			diff_y = price_y - curr_y
+			mod_x := diff_x % b_x
+			mod_y := diff_y % b_y
+			count_x = diff_x / b_x
+			count_y = diff_y / b_y
+
+			if mod_x == 0 && mod_y == 0 {
+				if first_i == -1 {
+					first_i = i
+					first_count_x = count_x
+					first_count_y = count_y
+					fmt.Printf("First i: %d - first x: %d - first y: %d\n", first_i, first_count_x, first_count_y)
+				} else {
+					delta_i = i - first_i
+					delta_count_x = count_x - first_count_x
+					delta_count_y = count_y - first_count_y
+
+					fmt.Printf("delta_i: %d - delta_x: %d - delta_y: %d\n", delta_i, delta_count_x, delta_count_y)
+
+					a1 := -delta_count_x + delta_count_y
+					a2 := -first_count_x + first_count_y
+					a3 := a2 / a1
+
+					fmt.Printf("a1: %d - a2: %d - a3: %d\n", a1, a2, a3)
+
+					button_a_count := first_i + int64(math.Abs(float64(a3*delta_i)))
+					button_a_count++
+
+					fmt.Printf("button_a_count: %d\n", button_a_count)
+
+					curr_x = a_x * button_a_count
+					curr_y = a_y * button_a_count
+
+					fmt.Printf("curr_x: %d - curr_y: %d \n", curr_x, curr_y)
+
+					diff_x = price_x - curr_x
+					diff_y = price_y - curr_y
+					count_x = diff_x / b_x
+					count_y = diff_y / b_y
+
+					fmt.Printf("count_x: %d count_y: %d\n", count_x, count_y)
+
+					total_x := curr_x + count_x*b_x
+					total_y := curr_y + count_y*b_y
+
+					fmt.Printf("Price_x: %d - Price_y: %d\n", price_x, price_y)
+					fmt.Printf("total_x: %d - total_y: %d\n", total_x, total_y)
+
+					if total_x == price_x && total_y == price_y && count_x == count_y {
+
+						fmt.Println("success")
+
+						curr_cost := button_a_count*3 + count_x
+						cost += curr_cost
+					}
+
+					break
+				}
+			}
+			// if mod_x == 0 && mod_y == 0 && count_y == count_x {
+			// 	fmt.Printf("price_x: %d price_y: %d\n", price_x, price_y)
+			// 	x := b_x * count_x
+			// 	y := b_y * count_y
+			// 	fmt.Printf("curr_x: %d curr_y: %d\n", curr_x+x, curr_y+y)
+			// 	break
+			// }
+
+			i++
+		}
+	}
+
+	return strconv.FormatInt(cost, 10)
 }
