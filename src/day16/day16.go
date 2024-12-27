9eeaf86fd9dedddb2c3f6590365a45e83763e1b1
Pekka Tuukkanen <pekka.tuukkanen@antura.se>
2024-12-27 19:17:51 +0100

    day16 part2

--- src/day16/day16.go
+++ src/day16/day16.go
@@ -4,58 +4,10 @@ import (
 	"fmt"
 	"slices"
 	"strconv"
-	"time"
 
 	"github.com/ptuukkan/aoc-2024/utils"
 )
 
-//	type Vertex struct {
-//		Key   utils.Point
-//		Edges map[*Vertex]int
-//	}
-//
-//	func newVertex(key utils.Point) *Vertex {
-//		return &Vertex{Key: key, Edges: make(map[*Vertex]int)}
-//	}
-//
-//	type Graph struct {
-//		Vertices map[utils.Point]*Vertex
-//	}
-//
-//	func (g *Graph) dijkstra(startKey utils.Point) (map[utils.Point]int, error) {
-//		if _, ok := g.Vertices[startKey]; !ok {
-//			return nil, fmt.Errorf("start vertex %v not found", startKey)
-//		}
-//
-//		distances := make(map[utils.Point]int)
-//		for key := range g.Vertices {
-//			distances[key] = math.MaxInt32
-//		}
-//		distances[startKey] = 0
-//
-//		var vertices []*Vertex
-//		for _, vertex := range g.Vertices {
-//			vertices = append(vertices, vertex)
-//		}
-//
-//		for len(vertices) != 0 {
-//			sort.SliceStable(vertices, func(i, j int) bool {
-//				return distances[vertices[i].Key] < distances[vertices[j].Key]
-//			})
-//
-//			vertex := vertices[0]
-//			vertices = vertices[1:]
-//
-//			for adjacent, cost := range vertex.Edges {
-//				alt := distances[vertex.Key] + cost
-//				if alt < distances[adjacent.Key] {
-//					distances[adjacent.Key] = alt
-//				}
-//			}
-//		}
-//
-//		return distances, nil
-//	}
 func findRune(lines []string, target rune) utils.Point {
 	for y, line := range lines {
 		for x, r := range line {
@@ -69,109 +21,15 @@ func findRune(lines []string, target rune) utils.Point {
 	return utils.NewPoint(0, 0)
 }
 
-//
-// func findEdges(maze []string, pos utils.Point, dir utils.Point) []utils.Point {
-// 	var edges []utils.Point
-//
-// 	for _, d := range utils.Directions {
-// 		p := pos.Add(d)
-// 		if pos.Subtract(&p) == dir {
-// 			continue
-// 		}
-// 		if maze[p.Y][p.X] != '#' {
-// 			edges = append(edges, p)
-// 		}
-// 	}
-//
-// 	return edges
-// }
-//
-// func createVertices(maze []string, graph *Graph, pos utils.Point, dir utils.Point) {
-// 	fmt.Printf("Y: %d - X: %d\n", pos.Y, pos.X)
-// 	// time.Sleep(1000 * time.Millisecond)
-// 	var vertex *Vertex
-// 	if elem, ok := graph.Vertices[pos]; ok {
-// 		vertex = elem
-// 	} else {
-// 		vertex = newVertex(pos)
-// 		graph.Vertices[pos] = vertex
-// 	}
-//
-// 	edges := findEdges(maze, pos, dir)
-// 	fmt.Printf("Possible Edges: %v\n", edges)
-// 	for _, edge := range edges {
-// 		var edgeVertex *Vertex
-// 		cost := 1
-// 		edgeDirection := edge.Subtract(&pos)
-// 		if edgeDirection != dir {
-// 			cost = 1001
-// 		}
-//
-// 		if elem, ok := graph.Vertices[edge]; ok {
-// 			fmt.Printf("Vertex %v already exists\n", edge)
-// 			edgeVertex = elem
-// 		} else {
-// 			fmt.Printf("Vertex %v does not exist\n", edge)
-// 			edgeVertex = newVertex(edge)
-// 			graph.Vertices[edge] = edgeVertex
-// 			createVertices(maze, graph, edge, edgeDirection)
-// 		}
-//
-// 		if _, ok := vertex.Edges[edgeVertex]; !ok {
-// 			fmt.Printf("New edge: %v\n", edge)
-// 			fmt.Printf("Pos %v - Edge %v - Edge dir %v - dir %v - cost %d\n", pos, edge, edgeDirection, dir, cost)
-// 			vertex.Edges[edgeVertex] = cost
-// 		}
-// 	}
-// }
-//
-// func createGraph(input string) (*Graph, utils.Point, utils.Point) {
-// 	maze := utils.SplitNewLines(input)
-// 	graph := &Graph{Vertices: make(map[utils.Point]*Vertex)}
-// 	startPos := findRune(maze, 'S')
-// 	endPos := findRune(maze, 'E')
-//
-// 	createVertices(maze, graph, startPos, utils.Directions[1])
-//
-// 	return graph, startPos, endPos
-// }
-
-// func Part1(input string) string {
-// 	graph, start, end := createGraph(input)
-//
-// 	distances, err := graph.dijkstra(start)
-// 	if err != nil {
-// 		return err.Error()
-// 	}
-//
-// 	endCost := distances[end]
-//
-// 	var points []utils.Point
-// 	for key := range distances {
-// 		points = append(points, key)
-// 		// fmt.Printf("y: %d - x: %d - cost: %d\n", key.Y, key.X, value)
-// 	}
-// 	slices.SortFunc(points, func(i, j utils.Point) int {
-// 		return distances[i] - distances[j]
-// 	})
-//
-// 	for _, p := range points {
-//
-// 		fmt.Printf("y: %d - x: %d - cost: %d\n", p.Y, p.X, distances[p])
-// 	}
-//
-// 	return strconv.Itoa(endCost)
-// }
-
 type Vertex struct {
 	Position  utils.Point
 	Direction utils.Point
 	Cost      int
-	Trail     []utils.Point
+	Prev      *Vertex
 }
 
-func newVertex(p utils.Point, d utils.Point, cost int, trail []utils.Point) Vertex {
-	return Vertex{Position: p, Direction: d, Cost: cost, Trail: trail}
+func newVertex(p utils.Point, d utils.Point, cost int, prev Vertex) Vertex {
+	return Vertex{Position: p, Direction: d, Cost: cost, Prev: &prev}
 }
 
 func queueAdj(maze []string, vertex Vertex, queue *[]Vertex, visited *[]Vertex) {
@@ -185,36 +43,30 @@ func queueAdj(maze []string, vertex Vertex, queue *[]Vertex, visited *[]Vertex) 
 			cost += 1000
 		}
 
-		// if slices.ContainsFunc(*queue, func(v Vertex) bool {
-		// 	return v.Position == adj && v.Cost < cost && v.Direction == dir
-		// }) {
-		// 	continue
-		// }
-
-		if slices.ContainsFunc(*visited, func(v Vertex) bool {
-			return v.Position == adj && v.Cost <= cost && v.Direction == dir
+		if slices.ContainsFunc(*queue, func(v Vertex) bool {
+			return v.Position == adj && v.Cost <= cost
 		}) {
 			continue
 		}
 
-		trail := make([]utils.Point, len(vertex.Trail))
-		copy(trail, vertex.Trail)
-		trail = append(trail, vertex.Position)
+		if slices.ContainsFunc(*visited, func(v Vertex) bool {
+			return v.Position == adj && v.Cost <= cost
+		}) {
+			continue
+		}
 
-		*queue = append(*queue, newVertex(adj, dir, cost, trail))
+		*queue = append(*queue, newVertex(adj, dir, cost, vertex))
 	}
-
 }
 
-func Part1(input string) string {
-	maze := utils.SplitNewLines(input)
+func findBestPath(maze []string) *Vertex {
 	start := findRune(maze, 'S')
 	end := findRune(maze, 'E')
 
 	var queue []Vertex
 	var visited []Vertex
 
-	vertex := Vertex{Position: start, Direction: utils.Directions[1], Cost: 0}
+	vertex := Vertex{Position: start, Direction: utils.Directions[1], Cost: 0, Prev: nil}
 
 	for {
 		visited = append(visited, vertex)
@@ -230,9 +82,17 @@ func Part1(input string) string {
 		queue = queue[1:]
 	}
 
-	print(maze, vertex.Trail)
+	return &vertex
+}
 
-	return strconv.Itoa(vertex.Cost)
+func Part1(input string) string {
+	maze := utils.SplitNewLines(input)
+
+	path := findBestPath(maze)
+
+	print(maze, linkedListToSlice(path))
+
+	return strconv.Itoa(path.Cost)
 }
 
 func print(maze []string, trail []utils.Point) {
@@ -251,8 +111,54 @@ func print(maze []string, trail []utils.Point) {
 	}
 }
 
-func Part2(input string) string {
-	maze := utils.SplitNewLines(input)
+func queueAdjPart2(maze []string, vertex Vertex, queue *[]Vertex, visited *[]Vertex, bestPath map[utils.Point]*Vertex) {
+	for i, dir := range utils.Directions {
+		adj := vertex.Position.Add(dir)
+		if maze[adj.Y][adj.X] == '#' {
+			continue
+		}
+
+		if vertex.Position.Subtract(&adj) == vertex.Direction {
+			continue
+		}
+
+		cost := vertex.Cost + 1
+		if dir != vertex.Direction {
+			cost += 1000
+		}
+
+		elem, ok := bestPath[adj]
+		if ok && elem.Cost < cost && elem.Direction == dir {
+			continue
+		}
+
+		if slices.ContainsFunc(*queue, func(v Vertex) bool {
+			return v.Position == adj && v.Cost < cost && (v.Direction == dir || v.Direction == utils.Directions[(i+2)%4])
+		}) {
+			continue
+		}
+
+		if slices.ContainsFunc(*visited, func(v Vertex) bool {
+			return v.Position == adj && v.Cost < cost && (v.Direction == dir || v.Direction == utils.Directions[(i+2)%4])
+		}) {
+			continue
+		}
+
+		*queue = append(*queue, newVertex(adj, dir, cost, vertex))
+	}
+}
+
+func vertexToPoint(vertices []Vertex) []utils.Point {
+	result := []utils.Point{}
+
+	for _, v := range vertices {
+		result = append(result, v.Position)
+	}
+
+	return result
+}
+
+func findAllBestPaths(maze []string, bestPath map[utils.Point]*Vertex) []Vertex {
 	start := findRune(maze, 'S')
 	end := findRune(maze, 'E')
 
@@ -262,12 +168,9 @@ func Part2(input string) string {
 	vertex := Vertex{Position: start, Direction: utils.Directions[1], Cost: 0}
 
 	for {
-		fmt.Println("----------------------------")
-		for _, q := range queue {
-			fmt.Printf("%v - %v - %d - %v\n\n", q.Position, q.Direction, q.Cost, q.Trail)
-		}
+		// print(maze, vertexToPoint(visited))
 		visited = append(visited, vertex)
-		queueAdj(maze, vertex, &queue, &visited)
+		queueAdjPart2(maze, vertex, &queue, &visited, bestPath)
 		if len(queue) == 0 || vertex.Position == end {
 			break
 		}
@@ -277,28 +180,49 @@ func Part2(input string) string {
 
 		vertex = queue[0]
 		queue = queue[1:]
-		time.Sleep(10 * time.Millisecond)
 	}
 
-	bestTiles := make(map[utils.Point]bool)
-
-	for _, t := range vertex.Trail {
-		bestTiles[t] = true
-	}
-	print(maze, vertex.Trail)
-	bestTiles[vertex.Position] = true
-
-	for _, v := range queue {
-		if v.Cost == vertex.Cost {
-			fmt.Println("---------------------")
-			fmt.Println(v.Cost)
-			print(maze, v.Trail)
-			for _, t := range v.Trail {
-				bestTiles[t] = true
-			}
-			bestTiles[v.Position] = true
+	results := []Vertex{vertex}
+	for _, q := range queue {
+		if q.Cost == vertex.Cost && q.Position == vertex.Position {
+			results = append(results, q)
 		}
 	}
 
-	return strconv.Itoa(len(bestTiles))
+	return results
+}
+
+func Part2(input string) string {
+	maze := utils.SplitNewLines(input)
+	path := findBestPath(maze)
+	trail := make(map[utils.Point]*Vertex)
+	for path != nil {
+		trail[path.Position] = path
+		path = path.Prev
+	}
+
+	paths := findAllBestPaths(maze, trail)
+
+	result := make(map[utils.Point]bool)
+	for _, p := range paths {
+		print(maze, linkedListToSlice(&p))
+		v := &p
+		for v != nil {
+			result[v.Position] = true
+			v = v.Prev
+		}
+	}
+
+	return fmt.Sprint(len(result))
+}
+
+func linkedListToSlice(vertex *Vertex) []utils.Point {
+	result := []utils.Point{}
+
+	for vertex != nil {
+		result = append(result, vertex.Position)
+		vertex = vertex.Prev
+	}
+
+	return result
 }
